2718bfb26db67611e94d67dbbca7a2e6668c4b10
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-12 02:00:18 +0000

    Add readme and use json! macro

---
+++ Readme.md
@@ -0,0 +1,5 @@
+- Serialize and Deserialize unstructured json
+
+TODO: 
+---
+- [ ] Use Deserialize macro for json
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use serde_json::{json, Error, Value};
+
+fn main() -> Result<(), Error> {
+    let json_data = r#"{
+        "id": 1, 
+        "title": "iPhone 9",
+        "description": "An apple mobile which is nothing like apple",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "stock": 94,
+        "brand": "Apple",
+        "category": "smartphones",
+        "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+      }"#;
+
+    let parsed_json: Value = serde_json::from_str(json_data)?;
+
+    println!("{:?}", parsed_json);
+
+    // generates a serde json value from a string
+    let _ = json!({   "id": 1, 
+    "title": "iPhone 9",
+    "description": "An apple mobile which is nothing like apple",
+    "price": 549,
+    "discountPercentage": 12.96,
+    "rating": 4.69,
+    "stock": 94,
+    "brand": "Apple",
+    "category": "smartphones",
+    "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg"});
+
+    Ok(())
 }
